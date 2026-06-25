725bfa46c93b5eb07a34fbe9f4210f0c9e6e48a4
Rodrigo Castro <rodpcastro18@gmail.com>
2026-06-24 21:25:49 -0300

    removed testing function

--- src/WaveGreen2D.jl
+++ src/WaveGreen2D.jl
@@ -3,13 +3,4 @@ module WaveGreen2D
 include("Chebyshev/Chebyshev.jl")
 using .Chebyshev
 
-"""
-    function testing(a::T, b::T) where T
-
-Testing function.
-"""
-function testing(a::T, b::T) where T
-    return a + b
-end
-
 end
