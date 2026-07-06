ddedce5c22610ac61f67fca7b7f717288562c4c3
Rodrigo Castro <rodpcastro18@gmail.com>
2026-07-06 03:06:12 -0300

    preparing wave parameters

--- src/WaveGreen2D.jl
+++ src/WaveGreen2D.jl
@@ -3,6 +3,10 @@ module WaveGreen2D
 using StaticArrays: SVector, SMatrix, MMatrix, SArray, Size
 
 
+include("wave_params.jl")
+
+
+# Submodules
 include("Chebyshev/Chebyshev.jl")
 include("NearField.jl")
 
---
+++ src/wave_params.jl
@@ -0,0 +1,82 @@
+mutable struct WaveParameters
+    depth::Float64
+    frequency::Float64
+    wavenumber::Float64
+    gravity::Float64
+
+    function WaveParameters(
+        depth::Real, frequency::Real, wavenumber::Real, gravity::Real
+    )
+        validate_wave(depth, frequency, wavenumber, gravity)
+        h = Float64(depth)
+        ω = Float64(frequency)
+        k₀ = Float64(wavenumber)
+        g = Float64(gravity)
+        return new(h, ω, k₀, g)
+    end
+end
+
+
+function validate_wave(
+    depth::Real, frequency::Real, wavenumber::Real, gravity::Real
+)
+    if depth ≤ 0
+        throw(DomainError(depth, "The depth must be positive."))
+    elseif frequency < 0
+        throw(DomainError(frequency, "The frequency must be non-negative."))
+    elseif wavenumber < 0
+        throw(DomainError(wavenumber, "The wavenumber must be non-negative."))
+    elseif gravity ≤ 0
+        throw(DomainError(gravity, "The acceleration of gravity must be positive."))
+    end
+end
+
+
+# Wave initializer
+const wave = WaveParameters(NaN, NaN, NaN, NaN)
+
+
+function setwave!(;
+    depth::Real,
+    frequency::Union{Real,Nothing}=nothing,
+    wavenumber::Union{Real,Nothing}=nothing,
+    gravity::Real=9.80665,
+)
+    h = Float64(depth)
+    g = Float64(gravity)
+
+    if !isnothing(frequency) && isnothing(wavenumber)
+        ω = Float64(frequency)
+        k₀ = ω^2 / g
+    elseif !isnothing(wavenumber) && isnothing(frequency)
+        k₀ = Float64(wavenumber)
+        ω = sqrt(k₀ * g * tanh(k₀ * h))
+    else
+        error("Either frequency or wavenumber must be given.")
+    end
+
+    validate_wave(h, ω, k₀, g)
+
+    wave.depth = h
+    wave.frequency = ω
+    wave.wavenumber = k₀
+    wave.gravity = g
+
+    return wave
+end
+
+
+# Reading aliases
+function Base.getproperty(obj::WaveParameters, sym::Symbol)
+    if sym === :h
+        return getfield(obj, :depth)
+    elseif sym === :ω
+        return getfield(obj, :frequency)
+    elseif sym === :k₀
+        return getfield(obj, :wavenumber)
+    elseif sym === :g
+        return getfield(obj, :gravity)
+    else
+        return getfield(obj, sym)  # Default fieldname
+    end
+end
