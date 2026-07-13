b5e9aa405ee22a7efeeb7f291742ba8c9eacc2c9
Documenter.jl <documenter@juliadocs.github.io>
2026-07-13 23:49:13 +0000

    build based on 3413e48

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"category":"section","location":"#WaveGreen2D","page":"Home","text":"Documentation for WaveGreen2D.\n\n","title":"WaveGreen2D"},{"category":"function","location":"#WaveGreen2D.setwave!","page":"Home","text":"function setwave!(depth::Real, frequency::Real, gravity::Real=9.80665) -> Nothing\n\nSets the parameters that define the environmental conditions.\n\n\n\n\n\n","title":"WaveGreen2D.setwave!"}]
+[{"category":"section","location":"#WaveGreen2D","page":"Home","text":"Documentation for WaveGreen2D.\n\n","title":"WaveGreen2D"},{"category":"function","location":"#WaveGreen2D.setwave!","page":"Home","text":"setwave!(depth::Real, frequency::Real, gravity::Real=9.80665) -> Nothing\n\nSets the parameters that define the environmental conditions.\n\n\n\n\n\n","title":"WaveGreen2D.setwave!"}]
 }
